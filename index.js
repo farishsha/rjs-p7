0cc8e0567b33b555b2ce83aeee6d74458b9741fa
farishbca <farishsha15@gmail.com>
2026-07-15 09:57:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 // Tag name: h1
 // Text: Welcome to React
 
-const element = React.CreateElement("h1",null,"Welcome to my page");
+const element = React.createElement("h1",null,"Welcome to my page");
 // TODO 2:
 // Create the React root
 
-const root = ReactDom.CreateRoot(document.getElement By Id ("root");
+const root = ReactDom.createroot(document.getElement By Id ("root");
 
 // TODO 3:
 // Render the element
